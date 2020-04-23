3644fe3e7f1df6d5b06a3ad0e4ab4676cb23b64b
Giovanny Lima da Silva <didiosilva395@gmail.com>
2020-04-22 23:12:13 -0400

    Adição de widget que irá retornar os produtos.

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual2/datas/product_data.dart';
+class ProducTile extends StatelessWidget {
+
+  final String type;
+  final ProductData data;
+
+  ProducTile(this.type,this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
