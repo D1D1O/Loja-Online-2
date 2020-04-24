aae15baa95af3b71a6228f7efd3c260601bbd9c9
Giovanny Lima da Silva <didiosilva395@gmail.com>
2020-04-23 23:51:00 -0400

    Adicionado Foto do produto,Tamanho e botão carrinho

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:lojavirtual2/datas/product_data.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
-
-
 class ProducScreen extends StatefulWidget {
 
   final ProductData product;
+
   ProducScreen(this.product);
 
   @override
@@ -15,35 +14,121 @@ class ProducScreen extends StatefulWidget {
 }
 
 class _ProducScreenState extends State<ProducScreen> {
-
   final ProductData product;
+
   _ProducScreenState(this.product);
+  String size;
 
   @override
   Widget build(BuildContext context) {
-    final primaryColor =  Theme.of(context).primaryColor;
+    final primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
       appBar: AppBar(
-        title: Text(product.title, style: TextStyle(fontSize: 16.0),),
+        title: Text(
+          product.title,
+          style: TextStyle(fontSize: 16.0),
+        ),
         centerTitle: true,
       ),
       body: ListView(
         children: <Widget>[
           AspectRatio(
-          aspectRatio: 0.9,
-          child: Carousel(
-            images: product.images.map((url){
-              return NetworkImage(url);
-            }).toList(),
-            dotSize: 4.0,
-            dotSpacing: 15.0,
-            dotBgColor: Colors.transparent,
-            dotColor: primaryColor,
-            autoplay: false,
-          ),)
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  "Tamanho",
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16.0),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.5
+                    ),
+                    children: product.sizes.map(
+                            (s){
+                              return GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    size = s;
+                                  });
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                                    border: Border.all(
+                                      color:s ==  size ? primaryColor : Colors.grey[500],
+                                      width: 3.0
+                                    )
+                                  ),
+                                  width: 50.0,
+                                  alignment: Alignment.center,
+                                  child: Text(s),
+                                ),
+                              );
+                            }
+                            ).toList(),
+                  ),
+                ),
+                SizedBox(height: 16.0,),
+                SizedBox(height: 44.0,
+                child: RaisedButton(
+                  onPressed: size != null ?(){}:null,
+                  child: Text("Adicionar ao Carrinho",
+                  style: TextStyle(fontSize: 18.0),
+                  ),
+                  color: primaryColor,
+                  textColor: Colors.white,
+                ),),
+                SizedBox(height: 16.0,),
+                Text(
+                  "Descrição",
+                  style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),
+                )
+
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 }
-
