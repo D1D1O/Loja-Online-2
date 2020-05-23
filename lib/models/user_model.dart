4ad1e117baa7627fdb0f23390859e6b06f8eb296
Giovanny Lima da Silva <didiosilva395@gmail.com>
2020-05-22 21:10:46 -0400

    Adicionado funcionalidade para o usuário logar no sistema.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,10 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:flutter/material.dart';
 import 'dart:async';
+
 class UserModel extends Model{
   bool isLoading = false;
+  FirebaseAuth _auth  = FirebaseAuth.instance;
+  FirebaseUser firebaseUser;
+  Map<String, dynamic>userData = Map();
 
-  void signUp(){
+  void signUp({@required Map<String,dynamic> userData,
+    @required String pass,@required VoidCallback onSuccess,@required VoidCallback onFail}){
+  isLoading = true;
+  notifyListeners();
+  _auth.createUserWithEmailAndPassword(
+      email: userData["email"],
+      password: pass).then((user) async{
+        firebaseUser = user;
+       await _saveUserData(userData);
 
+        onSuccess();
+        isLoading = false;
+        notifyListeners();
+  }).catchError((e){
+    onFail();
+    isLoading = false;
+    notifyListeners();
+  });
   }
   void signIn()async{
     isLoading = true;
@@ -18,6 +41,10 @@ class UserModel extends Model{
 
   void recoverPass(){
 
+  }
+  Future<Null> _saveUserData(Map<String,dynamic>userData) async{
+    this.userData = userData;
+    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
   }
 
 
